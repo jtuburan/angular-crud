d85d34222b26ab519ae9b18969b9ecc7604347e8
jason tuburan <jasontuburan@gmail.com>
2019-03-24 18:21:52 +0800

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// Firebase Modules
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
+
+// Reactive Form Module
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AddStudentComponent } from './add-student/add-student.component';
+import { EditStudentComponent } from './edit-student/edit-student.component';
+import { StudentListComponent } from './student-list/student-list.component';
+
+// NGX Pagination
+import { NgxPaginationModule } from 'ngx-pagination';
+
+// modules for NGX Toastr
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddStudentComponent,
+    EditStudentComponent,
+    StudentListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase), // Main Angular fire module 
+    AngularFireDatabaseModule,  // Firebase database module 
+    NgxPaginationModule,  // Include it in imports array
+    ReactiveFormsModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot() // ToastrModule added
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
